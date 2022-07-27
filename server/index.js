46adf2b0189c7f25f73911a63843c07bcc4d9451
jorovernier <55214971+jorovernier@users.noreply.github.com>
2022-07-27 11:24:39 -0700

    styling works with heroku

--- server/index.js
+++ server/index.js
@@ -4,18 +4,13 @@ const path = require('path');
 
 const app = express()
 
+
+// The index.html file has been moved into my public folder so /public needs to be added to the path string. 
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname, '../public/index.html'))
 })
 
-// app.get('/css', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../public/styles.css'))
-// })
-// app.get('/js', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../public/main.js'))
-// })
-
-
+// This is how we can connect the JS and CSS files so they can be accessed by Heroku.
 app.use(express.static(path.join(__dirname, '../public')))
 
 const port = process.env.PORT || 4005
